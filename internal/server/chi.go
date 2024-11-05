ebdc85b06d23f691d5bf5c5dc47d4a88882ef366
Meraiku <meraikuu@gmail.com>
2024-11-05 17:01:25 +0300

    Add logging to server

--- internal/server/chi.go
+++ internal/server/chi.go
@@ -4,11 +4,14 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func SetupHandlers() *chi.Mux {
 
 	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
 
 	r.Group(func(r chi.Router) {
 
